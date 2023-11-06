a0d0c0093690a779d009eb2d6a05dd2c65009151
baswaling <baswaling1993@gmail.com>
2023-11-06 19:44:59 +0530

    version 1.0.5

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def car_price():
 if __name__ == '__main__':
     
     # Running the application and leaving the debug mode ON
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, port=8080)
